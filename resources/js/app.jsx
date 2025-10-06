aff5e4060a298a34e14854ba09a0225adc160ef8
techapsalwar <tech@apsalwar.edu.in>
2025-10-06 14:41:59 +0530

    Fix: Correct import path case and upgrade Node.js to v20 for Vite 7

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
-import GlobalToastProvider from './components/GlobalToastProvider';
+import GlobalToastProvider from './Components/GlobalToastProvider';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Avhira';
 
